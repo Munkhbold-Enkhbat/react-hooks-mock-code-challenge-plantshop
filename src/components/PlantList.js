34c8cc261d189a02e5c0564a617c573a4cbf2e18
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-04-23 22:46:04 -0700

    Initiate app

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react/cjs/react.production.min";
 import PlantCard from "./PlantCard";
 
 function PlantList() {
+
+  const [plants, setPlants] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then(res => res.json())
+      .then(data => setPlants(data))
+  }, [])
+
+  function renderPlants() {
+    return plants.map(plant => {
+      return <PlantCard key={plant.id} plant={plant}/>
+    })
+  }
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {renderPlants()}
+    </ul>
   );
 }
 
